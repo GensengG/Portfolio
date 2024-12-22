ef6528e2d18ba2d935fc250fb4457de02739b825
GensengG <168531024+GensengG@users.noreply.github.com>
2024-12-22 22:07:34 +0300

    Update Toolbar.jsx

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,17 +1,24 @@
 import { useState } from "react";
-import { Portfolio } from "./Portfolio";
-import { ProjectList } from "./ProjectList.jsx";
 
-export const Toolbar = () => {
-    const sdbvskv = {
-        "All": ProjectList.first,
-        "Business Cards": ProjectList.second,
-        "Websites": ProjectList.third,
-        "Flayers": ProjectList.fourth,
-    }
-    let selected  = "";
-    const onSelectFilter = (selected) => {
-        return sdbvskv[selected];
-    };
+export const Toolbar = (holdCalback) => {
+    const {holdCalbackApp} = holdCalback;
+    return (
+        <>
+            <div className="toolbar">
+                <div className="toolbar__item" onClick = {holdCalbackApp}>
+                    {"All"}
+                </div>
+                <div className="toolbar__item" onClick = {holdCalbackApp}>
+                    {"Websites"}
+                </div>
+                <div className="toolbar__item" onClick = {holdCalbackApp}>
+                    {"Flayers"}  
+                </div>
+                <div className="toolbar__item" onClick = {holdCalbackApp}>
+                    {"Business Cards"}  
+                </div>
+            </div>
+        </>
+    );
 
 };
