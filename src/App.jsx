f3a2d0261ade5e14c2d7d39e2c196e5ba06c6930
GensengG <168531024+GensengG@users.noreply.github.com>
2024-12-28 18:44:15 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,163 +1,171 @@
 import './App.css';
 import { useState } from 'react';
 import { Portfolio } from "./components/Portfolio.jsx";
+import { PortfolioItem } from "./components/PortfolioItem.jsx";
 import { Toolbar } from "./components/Toolbar.jsx";
 
 function App() {
-  const state = {
-    itemCollection: [
-      {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/mon.jpg",
-          category: "Business Cards"
-      }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
-          category: "Websites"
-      }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg",
-          category: "Websites"
-      }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/codystretch.jpg",
-          category: "Websites"
-      }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_003.jpg",
-          category: "Business Cards"
-      }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290.png",
-          category: "Websites"
-      }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
-          category: "Websites"
-      }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg",
-          category: "Business Cards"
-      }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_1.png",
-          category: "Websites"
-      }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_2.png",
-          category: "Flayers"
-      }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/the_ninetys_brand.jpg",
-          category: "Websites"
-      }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/dia.jpg",
-          category: "Business Cards"
-      }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197.jpg",
-          category: "Websites"
-      }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg",
-          category: "Websites"
-      }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg",
-          category: "Business Cards"
-      }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197_1.jpg",
-          category: "Websites"
-      }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_3.png",
-          category: "Flayers"
-      }
-  ],
-    activeFilter: "All",
-    asd: [ 
-        <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/mon.jpg" />,
-        <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg"/>,
-        <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg"/>,
-        <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/codystretch.jpg"/>,
-        <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_003.jpg"/>,
-        <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290.png"/>,
-        <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg"/>,
-        <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg"/>,
-        <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_1.png"/>,
-        <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_2.png"/>,
-        <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/the_ninetys_brand.jpg"/>,
-        <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/dia.jpg"/>,
-        <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197.jpg"/>,
-        <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg"/>,
-        <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg"/>,
-        <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197_1.jpg"/>,
-        <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_3.png"/>,
-    ]
-  }  
-  
-  const filterAll = [
-  "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/mon.jpg",
-  "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
-  "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg",
-  "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/codystretch.jpg",
-  "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_003.jpg",
-  "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290.png",
-  "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
-  "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg",
-  "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_1.png",
-  "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_2.png",
-  "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/the_ninetys_brand.jpg",
-  "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/dia.jpg",
-  "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197.jpg",
-  "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg",
-  "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg",
-  "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197_1.jpg",
-  "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_3.png",
-  ];
-  const filterBusinessCards = [
-  "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/mon.jpg",
-  "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_003.jpg",
-  "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg",
-  "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/dia.jpg",
-  "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg",
-  ];
-  const filterWebsites = [
-    "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
-    "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg",
-    "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/codystretch.jpg",
-    "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290.png",
-    "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
-    "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_1.png",
-    "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/the_ninetys_brand.jpg",
-    "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197.jpg",
-    "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg",
-    "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197_1.jpg",
-  ];
-  const filterFlayers = [
-    "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_2.png",
-    "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_3.png",
-  ];
+    let state = {
+        currentItems: [
+            {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/mon.jpg",
+                category: "Business Cards",
+                id: "i1",
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
+                category: "Websites",
+                id: "i2"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg",
+                category: "Websites",
+                id: "i3"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/codystretch.jpg",
+                category: "Websites",
+                id: "i4"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_003.jpg",
+                category: "Business Cards",
+                id: "i5"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290.png",
+                category: "Websites",
+                id: "i6"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
+                category: "Websites",
+                id: "i7"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg",
+                category: "Business Cards",
+                id: "i8"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_1.png",
+                category: "Websites",
+                id: "i9"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_2.png",
+                category: "Flayers",
+                id: "i10"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/the_ninetys_brand.jpg",
+                category: "Websites",
+                id: "i11"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/dia.jpg",
+                category: "Business Cards",
+                id: "i12"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197.jpg",
+                category: "Websites",
+                id: "i13"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg",
+                category: "Websites",
+                id: "i14"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg",
+                category: "Business Cards",
+                id: "i15"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197_1.jpg",
+                category: "Websites",
+                id: "i16"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_3.png",
+                category: "Flayers",
+                id: "i17"
+            }
+        ],
+        itemCollection: [
+            {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/mon.jpg",
+                category: "Business Cards",
+                id: "i1",
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
+                category: "Websites",
+                id: "i2"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg",
+                category: "Websites",
+                id: "i3"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/codystretch.jpg",
+                category: "Websites",
+                id: "i4"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_003.jpg",
+                category: "Business Cards",
+                id: "i5"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290.png",
+                category: "Websites",
+                id: "i6"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
+                category: "Websites",
+                id: "i7"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg",
+                category: "Business Cards",
+                id: "i8"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_1.png",
+                category: "Websites",
+                id: "i9"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_2.png",
+                category: "Flayers",
+                id: "i10"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/the_ninetys_brand.jpg",
+                category: "Websites",
+                id: "i11"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/dia.jpg",
+                category: "Business Cards",
+                id: "i12"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197.jpg",
+                category: "Websites",
+                id: "i13"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg",
+                category: "Websites",
+                id: "i14"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg",
+                category: "Business Cards",
+                id: "i15"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197_1.jpg",
+                category: "Websites",
+                id: "i16"
+            }, {
+                img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_3.png",
+                category: "Flayers",
+                id: "i17"
+            }
+        ]
+    };
 
-  let filter = [];
-  let totalResult = [];
+    const [stateSwitch, setState] = useState(state); 
 
-  const [stateSwitch, setStateSwitch] = useState(state); 
+    function chooseCategory(category){
+        if(category === "All"){
+            setState({...stateSwitch}, stateSwitch.currentItems = stateSwitch.itemCollection)
+        } else {
+        setState({...stateSwitch}, stateSwitch.currentItems = stateSwitch.itemCollection.filter(el => el.category === category));
+        }
+    };
 
-  const holdCalback = (e) => {
-    filter = e.target.textContent;
-    totalResult = <Portfolio portfolioState={filter} />;
-
-    if (e.target.textContent === "All"){
-        setStateSwitch(stateSwitch.itemCollection = filterAll, stateSwitch.activeFilter = e.target.textContent, stateSwitch.asd = totalResult);
-    }
-    if (e.target.textContent === "Business Cards"){
-        setStateSwitch(stateSwitch.itemCollection = filterBusinessCards, stateSwitch.activeFilter = e.target.textContent, stateSwitch.asd = totalResult);
-    }
-    if (e.target.textContent === "Websites"){
-        setStateSwitch(stateSwitch.itemCollection = filterWebsites, stateSwitch.activeFilter = e.target.textContent, stateSwitch.asd = totalResult);
-    }
-    if (e.target.textContent === "Flayers"){
-        setStateSwitch(stateSwitch.itemCollection = filterFlayers, stateSwitch.activeFilter = e.target.textContent, stateSwitch.asd = totalResult);
-    }
-  }
-
-  return (
-    <>
-      <div>
-        <Toolbar holdCalbackApp={holdCalback} />
-      </div>
-      <div>
-        {stateSwitch.asd}
-      </div>
-    </>
-
-  )
+    return(
+        <div>
+            <Toolbar chooseFunk={chooseCategory}/>
+            <Portfolio itemsArr={stateSwitch}/>
+        </div>
+    );
 }
 
 export default App;
