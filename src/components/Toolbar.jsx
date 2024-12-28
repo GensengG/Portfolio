2d1769b2b0e971e7f7f70868e9ea65ca398fdc2a
GensengG <168531024+GensengG@users.noreply.github.com>
2024-12-28 18:41:00 +0300

    Update Toolbar.jsx

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,24 +1,37 @@
 import { useState } from "react";
+import "../App.jsx";
 
-export const Toolbar = (holdCalback) => {
-    const {holdCalbackApp} = holdCalback;
+export const Toolbar = (chooseCategory) => {
+
+    const {chooseFunk} = chooseCategory;
+
+    const state = {
+        filter: [
+            {
+                key: "All",
+                name: "All"
+            },
+            {
+                key: "Websites",
+                name: "Websites"
+            },
+            {
+                key: "Flayers",
+                name: "Flayers"
+            },
+            {
+                key: "Business Cards",
+                name: "Business Cards"
+            }
+        ]
+    }
+
     return (
-        <>
-            <div className="toolbar">
-                <div className="toolbar__item" onClick = {holdCalbackApp}>
-                    {"All"}
-                </div>
-                <div className="toolbar__item" onClick = {holdCalbackApp}>
-                    {"Websites"}
-                </div>
-                <div className="toolbar__item" onClick = {holdCalbackApp}>
-                    {"Flayers"}  
-                </div>
-                <div className="toolbar__item" onClick = {holdCalbackApp}>
-                    {"Business Cards"}  
-                </div>
-            </div>
-        </>
+        <div className="toolbar">
+            {state.filter.map(item => (
+                <div key={item.key} className="toolbar__item" onClick={() => chooseFunk(item.name)}>{item.name}</div>
+            ))}
+        </div>
     );
 
 };
