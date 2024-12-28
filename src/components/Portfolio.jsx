8e2fc7d87768599c3b787311c5d4cab9f7502e36
GensengG <168531024+GensengG@users.noreply.github.com>
2024-12-28 18:42:12 +0300

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,62 +1,16 @@
 import { useState } from "react";
+import { Toolbar } from "./Toolbar.jsx";
+import "../App.jsx";
+import { PortfolioItem } from "./PortfolioItem.jsx";
 
+export const Portfolio = (stateSwitch) => {
+    const {itemsArr} = stateSwitch;
 
-export const Portfolio = (filter) => {
-    let {portfolioState} = filter;
-    const result = [];
-    if(portfolioState === "All"){
-        result = [
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/mon.jpg" />,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg"/>,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg"/>,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/codystretch.jpg"/>,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_003.jpg"/>,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290.png"/>,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg"/>,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg"/>,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_1.png"/>,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_2.png"/>,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/the_ninetys_brand.jpg"/>,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/dia.jpg"/>,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197.jpg"/>,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg"/>,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg"/>,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197_1.jpg"/>,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_3.png"/>,
-        ]
-    }
-    if(portfolioState === "Business Cards"){
-        result = [
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/mon.jpg" />,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_003.jpg" />,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg" />,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/dia.jpg" />,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg" />,
-        ]
-    }
-    if(portfolioState === "Websites"){
-        result = [
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg" />,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg" />,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/codystretch.jpg" />,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290.png" />,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg" />,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_1.png" />,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/the_ninetys_brand.jpg" />,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197.jpg" />,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg" />,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197_1.jpg" />,
-        ]
-    }
-    if(portfolioState === "Flayers"){
-        result = [
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_2.png" />,
-            <img src="https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_3.png" />,
-        ]
-    }
-    return (
-        <>
-            {result}
-        </>
+    return(
+        <div className="portfolio">
+            itemsArr.currentItems.map(element => (
+                <PortfolioItem key={element.id} el={element}/>
+            ))}
+        </div>
     )
 }
